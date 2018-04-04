ea5788d7783e0ef204444d0074b1c28618169ba5
Atanas Kostovski <atanasmk16@gmail.com>
2018-04-04 15:10:19 +0200

    Added JWT Auth

--- travel-be/routes/api.php
+++ travel-be/routes/api.php
@@ -13,8 +13,36 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//get all locations for user
+Route::get('/{user}/locations', 'LocationController@index');
+
+//get selected location info for user
+Route::get('/{user}/locations/{location}', 'LocationController@show');
+
+//add new location
+Route::post('/{user}/locations', 'LocationController@store');
+
+//edit location with id for user
+Route::put('/{user}/locations/{location}', 'LocationController@update');
+
+//delete location with id for user
+Route::delete('/{user}/locations/{location}', 'LocationController@destroy');
 
--- travel-be/routes/web.php
+++ travel-be/routes/web.php
@@ -10,28 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-//get all locations for user
-Route::get('/{user}/locations', 'LocationController@index');
-
-//get selected location info for user
-Route::get('/{user}/locations/{location}', 'LocationController@show');
-
-//add new location
-Route::post('/{user}/locations', 'LocationController@store');
-
-//edit location with id for user
-Route::put('/{user}/locations/{location}', 'LocationController@update');
-
-
-//delete location with id for user
-Route::delete('/{user}/locations/{location}', 'LocationController@destroy');
-
-Auth::routes();
-
-
